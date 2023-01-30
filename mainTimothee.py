c3dedb633aaf7d72bdef3d065611b8a0c6f07fbb
TimotheeSvg <102729134+TimotheeSvg@users.noreply.github.com>
2023-01-30 20:21:59 +0100

    set symbol

--- mainTimothee.py
+++ mainTimothee.py
@@ -4,8 +4,16 @@ import time
 import multiprocessing
 import lib.timerLib as timeL
 
+
+def update_all_db():
+    update_data_features(symbols=get_symbol())
+
 if __name__ == '__main__': 
     start = time.time()
-    update_data_features(symbols=get_symbol())
+    l = get_symbol()
+    print("ok")
+    time.sleep(5)
+    save_symbol(l)
     timetab = timeL.get_time_from_ms(timeL.diff(start))
     print(f'MAIN time for process : {timetab[2]}h:{timetab[1]}m:{timetab[0]}s')
+
